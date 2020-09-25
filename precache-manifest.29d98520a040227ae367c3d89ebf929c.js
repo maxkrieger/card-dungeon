39ef8e4ee58f33ef7ede57409ddc1d7d7305d30d
maxkrieger <themaxaxis@gmail.com>
2020-09-24 20:59:08 -0400

    Updates

---
+++ precache-manifest.29d98520a040227ae367c3d89ebf929c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a9f2a2d30bc1f12d4a3e3b68a6cdd6",
+    "url": "/card-dungeon/index.html"
+  },
+  {
+    "revision": "763f2dff82bfba1b1ca8",
+    "url": "/card-dungeon/static/css/2.dcab9049.chunk.css"
+  },
+  {
+    "revision": "98e335d195ade3d72747",
+    "url": "/card-dungeon/static/css/main.a1c6ae05.chunk.css"
+  },
+  {
+    "revision": "763f2dff82bfba1b1ca8",
+    "url": "/card-dungeon/static/js/2.3c6f6fde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/card-dungeon/static/js/2.3c6f6fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e335d195ade3d72747",
+    "url": "/card-dungeon/static/js/main.a89bd3b2.chunk.js"
+  },
+  {
+    "revision": "5c7743d6d2f856949ca9",
+    "url": "/card-dungeon/static/js/runtime-main.b40dd721.js"
+  },
+  {
+    "revision": "6faad1789bcbd321a458c14d0735d976",
+    "url": "/card-dungeon/static/media/alagard.6faad178.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/card-dungeon/precache-manifest.713714bf77ca902211206887ab3658c9.js"
+  "/card-dungeon/precache-manifest.29d98520a040227ae367c3d89ebf929c.js"
 );
 
 self.addEventListener('message', (event) => {
